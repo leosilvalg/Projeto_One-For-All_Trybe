41a30ccfe9206bf70b5ebd0452af3101d48a0a3b
Leonardo Silva <leonardorc.geo@gmail.com>
2022-06-28 12:52:13 -0300

    Req 07

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+select 
+	a.artista as 'artista',
+    b.album as 'album',
+    count(c.artista_id) as 'seguidores'
+from SpotifyClone.artistas as a
+inner join SpotifyClone.albums as b
+on a.artista_id = b.artista_id
+inner join SpotifyClone.seguindo_artistas as c
+on c.artista_id = b.artista_id
+group by a.artista, b.album
+order by seguidores desc, artista asc, album asc;
