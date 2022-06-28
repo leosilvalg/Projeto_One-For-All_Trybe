79052760a118bf87eef40c79337a9a58fffa5377
Leonardo Silva <leonardorc.geo@gmail.com>
2022-06-28 12:29:08 -0300

    Req 03 e adiciona os outros arquivos

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+select a.usuario AS "usuarios",
+	count(b.cancao_id) AS "qtde_musicas_ouvidas",
+    round(sum(c.duracao) / 60, 2) AS "total_minutos"
+from SpotifyClone.usuarios AS a
+inner join SpotifyClone.historico_reproducoes AS b
+on a.usuario_id = b.usuario_id
+inner join SpotifyClone.cancoes as c
+on b.cancao_id = c.cancao_id
+group by a.usuario
