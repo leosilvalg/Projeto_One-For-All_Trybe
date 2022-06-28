e046a451392f9e4c0ad47f984430e9ad16eeebc3
Leonardo Silva <leonardorc.geo@gmail.com>
2022-06-28 12:47:44 -0300

    Fazendo as devidas correções nos reqs 3 e 4

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
-select a.usuario AS "usuarios",
-	count(b.cancao_id) AS "qtde_musicas_ouvidas",
-    round(sum(c.duracao) / 60, 2) AS "total_minutos"
+select a.usuario AS "usuario",
+count(b.cancao_id) AS "qtde_musicas_ouvidas",
+round(sum(c.duracao) / 60, 2) AS "total_minutos"
 from SpotifyClone.usuarios AS a
 inner join SpotifyClone.historico_reproducoes AS b
 on a.usuario_id = b.usuario_id
--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,9 @@
-select a.usuario AS "usuarios",
-	if (
-		max(year(b.data_reproducao)) >= '2021-01-01',
-			'Usuario ativo',
-			'Usuario inativo'
-	) as 'condicao_usuario'
+select a.usuario AS "usuario",
+if (
+max(year(b.data_reproducao)) >= '2021-01-01',
+'Usuário ativo',
+'Usuário inativo'
+) as 'condicao_usuario'
 from SpotifyClone.usuarios as a
 inner join SpotifyClone.historico_reproducoes as b
 on a.usuario_id = b.usuario_id
